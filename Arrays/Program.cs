40a0733a7d9c219bb65dfd2c9c6a23225fda3333
Mine3e <minememmedlii@gmail.com>
2024-03-12 02:10:47 +0400

    commits

---
+++ Arrays/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Xml.Linq;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //int[] arr = { 8, 5, 45 };
+            //int min = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < min)
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //Console.WriteLine(min);
+            #endregion
+
+            #region task2
+            //string[] arr = { "Salam", "sagol", "what" };
+            //int index =int.Parse(Console.ReadLine());
+            //string element = arr[index];
+            //string tersi = "";
+            //for(int i=element.Length - 1; i >= 0; i--)
+            //{
+            //    tersi += element[i];
+            //}
+            //Console.WriteLine(tersi);
+            #endregion
+
+            #region task3
+            //int[] arr = { -2, 3, 4, -5 };
+            //int[] tersi = new int[arr.Length];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] < 0)
+            //    {
+            //        tersi[i] = -arr[arr.Length-1-i];
+            //    }
+            //    else
+            //    {
+            //        tersi[i] = arr[arr.Length - 1 - i];
+            //    }
+            //    Console.Write(tersi[i]+" ");
+            //}
+            #endregion
+
+            #region task4
+            //int[] arr = { -2, 3, 4, -5, };
+            //string S = "";
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    S += arr[i].ToString()+" ";
+            //}
+            //Console.WriteLine(S);
+            #endregion
+
+        }
+    }
+}
